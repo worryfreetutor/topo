d1d76e4d97fad2fb6cbf70ccdb25690d74be4aec
csxiaocy <xiaochuyu@outlook.com>
2019-09-03 11:35:46 +0800

    更新了一点样式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,52 +12,60 @@
       </Radio>
     </RadioGroup>
     <br>
+    <br>
     <!-- 输入文本框 -->
     <Input v-model="inputText" v-show="inputType === 'text'"
       type="textarea" :autosize="{minRows: 2,maxRows: 6}" placeholder="请按格式输入数据..."
       @on-blur="checkInputText"
     />
+    <br>
     <!-- 上传文件框 -->
     <Upload
       type="drag"
       accept=".txt,.dat"
       action=""
       :before-upload="uploadFile"
-      v-show="inputType === 'file'"
-      style="width:400px;padding: 80px;">
+      v-show="inputType === 'file'">
       <div style="padding: 20px 0">
         <Icon type="ios-cloud-upload" size="52" style="color: #3399ff"></Icon>
         <p>点击或将文件拖拽到这里上传</p>
       </div>
     </Upload>
+    <br>
     <!-- 关系图 -->
     <b>关系图如下</b>
     <Card style="width:100%">
       <div id="graph" style="width:100%;height:600px"></div>
     </Card>
-    <b>result: {{ relationStr }}</b>
     <br>
     <!-- 选择高亮显示课程名 -->
-    <Select v-model="highLight" clearable style="width:200px">
+    <Select v-model="highLight" clearable style="width:200px;" v-show="result.length !== 0">
       <Option v-for="item in courseList" :value="item" :key="item">{{ item }}</Option>
     </Select>
+    <font>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</font>
+    <!-- 导出文件按钮 -->
+    <Button :size="buttonSize" icon="ios-download-outline" type="primary"
+      v-show="result.length !== 0" @click="downloadFile">导出数据文件</Button>
     <br>
-    <b>{{ highLight }}</b>
     <br>
     <!-- 输出文本框(高亮显示) -->
-    <div>
-      <div v-for="(item, index) in result" :key="index"  @click="showPathGraph(index)" style="cursor: pointer;">
+    <div style="border:1px solid #adcd3c;border-radius: 5px;background:#f2fddb" v-show="result.length !== 0">
+      <font>生成课程安排结果</font>
+      <div v-for="(item, index) in result" :key="index"  @click="showPathGraph(index)" style="cursor: pointer;font-size: 24px;">
+        <font>{{ index+1 }}&nbsp;&nbsp;</font>
         <font>{{ item[0].split(highLight)[0] }}</font>
         <font v-show="item[0].indexOf(highLight) !== -1"  style="color: red;">{{ highLight }}</font>
         <font>{{ item[0].split(highLight)[1] }}</font>
       </div>
     </div>
     <br>
-    <!-- 导出文件按钮 -->
-    <Button size="large" icon="ios-download-outline" type="primary"
-      v-show="relationStr !== ''" shape="circle" @click="downloadFile"></Button>
-    <b>路径图如下</b>
-    <Card style="width:100%">
+    <b v-show="result.length !== 0">路径图如下</b>
+    <Card style="width:100%" v-show="result.length !== 0">
       <div id="roadmap" style="width:100%;height:600px"></div>
     </Card>
   </div>
@@ -73,10 +81,7 @@ export default {
       inputText: '',  // 输入框输入的内容
       inputType: 'text',  // text or file 输入的类型
       relationStr: '',  // 经过处理的节点关系数据
-      result: [
-        'a->b->c',
-        'd->e->f'
-      ],  // 结果数组
+      result: [],  // 结果数组
       // 不会起变量名 T_T
       courseList: [], // 所有课程的数组
       highLight: ' ',  // 高亮显示的课程名，初始值为空格!
